f98ff70be2caaf4b97087cb0cd34d279478da927
ImGreenWolf <77302628+ImGreenWolf@users.noreply.github.com>
2026-06-16 11:45:20 +0300

    Update Media.ts

--- triumph-website/src/collections/Media.ts
+++ triumph-website/src/collections/Media.ts
@@ -44,7 +44,8 @@ export const Media: CollectionConfig = {
   ],
   upload: {
     // Upload to the public/media directory in Next.js making them publicly accessible even outside of Payload
-    staticDir: path.resolve(dirname, '../../public/media'),
+    staticDir:  process.env.PAYLOAD_MEDIA_DIR ||
+    path.resolve(process.cwd(), 'public/media'),//path.resolve(dirname, '../../public/media'),
     adminThumbnail: 'thumbnail',
     focalPoint: true,
     imageSizes: [
